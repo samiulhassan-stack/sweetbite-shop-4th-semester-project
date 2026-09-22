03fa7dfe34dba5f477d4e52e863da9c3d6eb5619
Sami Hassan <ok7446124@gmail.com>
2026-09-22 12:59:46 +0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sweetbite-shop-4th-semester-project/',
   plugins: [react(), tailwindcss()],
 })
